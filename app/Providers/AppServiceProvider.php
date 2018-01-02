d4057e52ab4d8712f5906b8a462b31fdd7c587ee
Maxwell Kenned <kenned123@gmail.com>
2018-01-02 00:47:06 -0300

    Webservice

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $url->forceSchema('https');
+      if(env('APP_ENV') !== 'local')
+      {
+          $url->forceSchema('https');
+      }
     }
 
     /**
